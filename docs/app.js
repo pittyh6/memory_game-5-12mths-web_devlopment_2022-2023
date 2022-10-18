8af55c0d5fdfe23cf76a499a181d3f12d3ac4921
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-18 13:00:29 +1100

    add img sizes

--- docs/app.js
+++ docs/app.js
@@ -10,7 +10,7 @@ console.log(getAllBlock.length)
 for(let i = 0; i < getAllBlock.length; i++){
     let elem = document.createElement("img");
 elem.setAttribute("src", "../docs/img/angel.png")
-elem.setAttribute("width", "100%")
+// elem.setAttribute("width", "100%")
     console.log(getAllBlock[i])
    getAllBlock[i].appendChild(elem)
 }
