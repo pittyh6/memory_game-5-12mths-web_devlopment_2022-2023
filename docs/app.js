dc82190b25d8e35ba1514f75a68245844222a167
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-19 09:29:54 +1100

    new version layout, time and btn, background

--- docs/app.js
+++ docs/app.js
@@ -1,16 +1,10 @@
-// let elem = document.createElement("img");
+
+// let getAllBlock = document.querySelectorAll(".img-block")
+// console.log(getAllBlock.length)
+// for(let i = 0; i < getAllBlock.length; i++){
+//     let elem = document.createElement("img");
 // elem.setAttribute("src", "../docs/img/angel.png")
-// elem.setAttribute("width", "100%")
-
-
-
-let getAllBlock = document.querySelectorAll(".img-block")
-//document.querySelectorAll(".img-block").appendChild(elem)
-console.log(getAllBlock.length)
-for(let i = 0; i < getAllBlock.length; i++){
-    let elem = document.createElement("img");
-elem.setAttribute("src", "../docs/img/angel.png")
-// elem.setAttribute("width", "100%")
-    console.log(getAllBlock[i])
-   getAllBlock[i].appendChild(elem)
-}
+// // elem.setAttribute("width", "100%")
+//     console.log(getAllBlock[i])
+//    getAllBlock[i].appendChild(elem)
+// }
--- style-guide.md
+++ style-guide.md
@@ -1,7 +1,21 @@
-# Color:
-* white #fff
-* Light orange #f5d6bc
-* dark marron #9d7474
+# Color background linear:
+* dark-blue: rgb(3, 2, 38), 
+* medium-blue: rgb(32, 28, 103)
+* light-blue: rgb(61, 58, 129)
 
+
+https://colorhunt.co/palette/1c3879607eaaeae3d2f9f5eb
+# Tons Blue: 
+* dark-blue: #1C3879
+* medium-blue: #607EAA
+# Beige
+* dark-beige: #EAE3D2
+* light-beige: #F9F5EB
+
+https://colorhunt.co/palette/1b243051557e816797d6d5a8
+* almost-black: #1B2430
+* medium-blue-purple: #51557E
+* medium-purple: #816797
+* light-green-yellow: #D6D5A8
 # Font: 
 * Sans-serif
