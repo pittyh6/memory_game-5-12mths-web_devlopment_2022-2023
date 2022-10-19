b6d860dbf5c81840621c4df118a59ca1987cf1c0
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-20 10:43:31 +1100

    click button start add imgs

--- docs/app.js
+++ docs/app.js
@@ -1,10 +1,19 @@
 
 let getAllBlock = document.querySelectorAll(".img-block")
 console.log(getAllBlock.length)
-for (let i = 0; i < getAllBlock.length; i++) {
-    let elem = document.createElement("img");
-    elem.setAttribute("src", "../docs/img/angel.png")
-    elem.setAttribute("width", "100%")
-    console.log(getAllBlock[i])
-    getAllBlock[i].appendChild(elem)
-}
+let btnStart = document.querySelector(".btn-start")
+btnStart.addEventListener("click",function(){
+    if(btnStart.classList.contains("hasImg")){
+        console.log("has image")
+    }else{
+        $(this).addClass("hasImg")
+        for (let i = 0; i < getAllBlock.length; i++) {
+            let elem = document.createElement("img");
+            elem.setAttribute("src", "../docs/img/angel.png")
+            elem.setAttribute("width", "100%")
+            console.log(getAllBlock[i])
+            getAllBlock[i].appendChild(elem)
+        }
+    }
+
+} )
