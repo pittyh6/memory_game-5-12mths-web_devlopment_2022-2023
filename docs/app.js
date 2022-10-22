ff07d839a531584c1da867d1b84bba6020af700f
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-22 19:15:51 +1100

    double photo done

--- docs/app.js
+++ docs/app.js
@@ -5,6 +5,12 @@ images[2] = '../docs/img/fart.png'
 images[3] = '../docs/img/fearful.png'
 images[4] = '../docs/img/hearts-eyes.png'
 images[5] = '../docs/img/hobby.png'
+images[6] = '../docs/img/angel-2.png'
+images[7] = '../docs/img/sunglasses-2.png'
+images[8] = '../docs/img/fart-2.png'
+images[9] = '../docs/img/fearful-2.png'
+images[10] = '../docs/img/hearts-eyes-2.png'
+images[11] = '../docs/img/hobby-2.png'
 // images[6] = '../docs/img/sad-crying.png'
 // images[7] = '../docs/img/cat.png'
 // images[8] = '../docs/img/tears-of-joy.png'
@@ -22,21 +28,23 @@ btnStart.addEventListener("click",function(){
         console.log("has image class")
     }else{
         $(this).addClass("hasImg")
-        let newImgRandomArr = []
+        let newPositionRandomArr = [] // change for not random img but random position..... If full find next empty spot...
         for (let i = 0; i < getAllBlock.length; i++) {
             let elem = document.createElement("img");
-            let randomNumberImage = images[Math.floor(Math.random()* 6)]
-            if(newImgRandomArr.includes(randomNumberImage)){
+            let randomPosition = Math.floor(Math.random()* 12)
+            console.log(randomPosition)
+            if(newPositionRandomArr[randomPosition] == null && newPositionRandomArr[randomPosition] == 'undefined'){
                 console.log('img already added')
             }else{
-                newImgRandomArr.push(randomNumberImage)
-                elem.setAttribute("src", randomNumberImage)
+                newPositionRandomArr.push(images[i])
+                elem.setAttribute("src", images[i])
                 elem.setAttribute("width", "100%")
                 console.log(getAllBlock[i])
-                getAllBlock[i].appendChild(elem)
+                getAllBlock[randomPosition].appendChild(elem)
             }
         }
-        console.log(newImgRandomArr)
+        console.log(newPositionRandomArr)
+
     }
 
 } )
