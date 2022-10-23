8a7b78ef1a462edb9cb4bb05d57d4f0beac0ab51
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-23 14:47:11 +1100

    get src from img and set for only two clicks

--- docs/app.js
+++ docs/app.js
@@ -1,16 +1,16 @@
 const images = new Array()
-images[0] = '../docs/img/angel.png'
-images[1] = '../docs/img/sunglasses.png'
-images[2] = '../docs/img/fart.png'
-images[3] = '../docs/img/fearful.png'
-images[4] = '../docs/img/hearts-eyes.png'
-images[5] = '../docs/img/hobby.png'
-images[6] = '../docs/img/angel-2.png'
-images[7] = '../docs/img/sunglasses-2.png'
-images[8] = '../docs/img/fart-2.png'
-images[9] = '../docs/img/fearful-2.png'
-images[10] = '../docs/img/hearts-eyes-2.png'
-images[11] = '../docs/img/hobby-2.png'
+images[0] = 'img/angel.png'
+images[1] = 'img/sunglasses.png'
+images[2] = 'img/fart.png'
+images[3] = 'img/fearful.png'
+images[4] = 'img/hearts-eyes.png'
+images[5] = 'img/hobby.png'
+images[6] = 'img/angel.png'
+images[7] = 'img/sunglasses.png'
+images[8] = 'img/fart.png'
+images[9] = 'img/fearful.png'
+images[10] = 'img/hearts-eyes.png'
+images[11] = 'img/hobby.png'
 // images[6] = '../docs/img/sad-crying.png'
 // images[7] = '../docs/img/cat.png'
 // images[8] = '../docs/img/tears-of-joy.png'
@@ -20,9 +20,11 @@ images[11] = '../docs/img/hobby-2.png'
    
 
 
-
+//content save elements
 let getAllBlock = document.querySelectorAll(".img-block")
 let btnStart = document.querySelector(".btn-start")
+let newImgArr = []
+
 
 //ADD IMG TO THE DIVS WHEN START BUTTON IS PRESSED.
 btnStart.addEventListener("click",function(){
@@ -35,9 +37,9 @@ btnStart.addEventListener("click",function(){
             let elem = document.createElement("img");
             let randomPosition = Math.floor(Math.random()* 12)
             console.log(randomPosition)
-            if(newPositionRandomArr[randomPosition] == null && newPositionRandomArr[randomPosition] == 'undefined'){
-                console.log('img already added')
-            }else{//check if position already has been used.
+            // if(newPositionRandomArr[randomPosition] == null && newPositionRandomArr[randomPosition] == 'undefined'){
+            //     console.log('img already added')
+            // }else{//check if position already has being used.
                 if(newPositionRandomArr.includes(randomPosition)){
                     console.log("contain possition: " + randomPosition)
                     i--
@@ -47,11 +49,28 @@ btnStart.addEventListener("click",function(){
                     elem.setAttribute("width", "100%")
                     console.log(getAllBlock[i])
                     getAllBlock[randomPosition].appendChild(elem)
+                    newImgArr.push(elem,getAllBlock[i])
                 }
-            }
+            // }
         }
         console.log(newPositionRandomArr)
-
+        console.log(newImgArr[0])
     }
-
 } )
+
+
+// verify that 2 images are the same.
+let count = 1
+document.querySelector('.bottom-board').addEventListener("click", function(e){
+    let firstClick, secondClick;
+    if(count == 1){
+        firstClick = e.target.src
+        console.log("clicked image. First: " + firstClick)
+        count ++
+    }else{
+        secondClick = e.target.src
+        console.log("clicked image. Second: " + secondClick)
+        count = 1
+    }
+    
+})
