084ff5e11a420f8586c32176056f85967bdce2f7
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-10-23 14:59:31 +1100

    check if images are the same

--- docs/app.js
+++ docs/app.js
@@ -17,7 +17,7 @@ images[11] = 'img/hobby.png'
 // images[9] = '../docs/img/tongue-out.png'
 // images[10] = '../docs/img/unamused.png'
 // images[11] = '../docs/img/wink.png'
-   
+
 
 
 //content save elements
@@ -27,50 +27,57 @@ let newImgArr = []
 
 
 //ADD IMG TO THE DIVS WHEN START BUTTON IS PRESSED.
-btnStart.addEventListener("click",function(){
-    if(btnStart.classList.contains("hasImg")){
+btnStart.addEventListener("click", function () {
+    if (btnStart.classList.contains("hasImg")) {
         console.log("has image class")
-    }else{
+    } else {
         $(this).addClass("hasImg")
         let newPositionRandomArr = [] // change for not random img but random position..... If full , genarate a new random number until find a free spot
         for (let i = 0; i < getAllBlock.length; i++) {
             let elem = document.createElement("img");
-            let randomPosition = Math.floor(Math.random()* 12)
+            let randomPosition = Math.floor(Math.random() * 12)
             console.log(randomPosition)
             // if(newPositionRandomArr[randomPosition] == null && newPositionRandomArr[randomPosition] == 'undefined'){
             //     console.log('img already added')
             // }else{//check if position already has being used.
-                if(newPositionRandomArr.includes(randomPosition)){
-                    console.log("contain possition: " + randomPosition)
-                    i--
-                }else{
-                    newPositionRandomArr.push(randomPosition)
-                    elem.setAttribute("src", images[i])
-                    elem.setAttribute("width", "100%")
-                    console.log(getAllBlock[i])
-                    getAllBlock[randomPosition].appendChild(elem)
-                    newImgArr.push(elem,getAllBlock[i])
-                }
+            if (newPositionRandomArr.includes(randomPosition)) {
+                console.log("contain possition: " + randomPosition)
+                i--
+            } else {
+                newPositionRandomArr.push(randomPosition)
+                elem.setAttribute("src", images[i])
+                elem.setAttribute("width", "100%")
+                console.log(getAllBlock[i])
+                getAllBlock[randomPosition].appendChild(elem)
+                newImgArr.push(elem, getAllBlock[i])
+            }
             // }
         }
         console.log(newPositionRandomArr)
         console.log(newImgArr[0])
     }
-} )
+})
 
 
 // verify that 2 images are the same.
 let count = 1
-document.querySelector('.bottom-board').addEventListener("click", function(e){
-    let firstClick, secondClick;
-    if(count == 1){
+let firstClick
+let secondClick
+document.querySelector('.bottom-board').addEventListener("click", function (e) {
+    if (count == 1) {
         firstClick = e.target.src
         console.log("clicked image. First: " + firstClick)
-        count ++
-    }else{
+        count = 2
+        // return firstClick
+    } else {
         secondClick = e.target.src
         console.log("clicked image. Second: " + secondClick)
         count = 1
+        console.log("check first value: " + firstClick)
+        if (firstClick == secondClick) {
+            console.log("MATCHHHHH")
+        } else {
+            console.log("Yeah, Nah.")
+        }
     }
-    
 })
